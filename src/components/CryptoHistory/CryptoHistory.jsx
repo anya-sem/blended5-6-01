6ec5bf7f8270aeea9e8627172a35250a6433f0bd
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2024-01-21 12:05:21 +0100

    update proj

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,30 @@
-// const tableHeaders = ['№', 'price', 'amount', 'date'];
+const tableHeaders = ['№', 'price', 'amount', 'date'];
+import { formatData } from 'helpers/formatData';
+import style from './CryptoHistory.module.css';
 
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          {tableHeaders.map((item, index) => (
+            <th className={style.th} key={index}>
+              {item}
+            </th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }, index) => (
+          <tr className={style.tr} key={id}>
+            <td className={style.td}>{index + 1}</td>
+            <td className={style.td}>{price}</td>
+            <td className={style.td}>{amount}</td>
+            <td className={style.td}>{formatData(date)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
