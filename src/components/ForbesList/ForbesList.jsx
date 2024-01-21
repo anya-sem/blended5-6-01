dcc5ed2547c5de4e7f19bf7333a3682937182d50
Dmytro20777 <08dmutro11@gmail.com>
2024-01-21 12:47:34 +0200

    Task3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,15 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import { ForbesListItem } from "components/ForbesListItem/ForbesListItem";
+import style from "./ForbesList.module.css"
+
+export const ForbesList = ({ list }) => {
+  return <div className={style.board}>
+  <div className={style.header}>
+    <h2 className={style.title}>
+      <span className={style.titleTop}>Forbes</span>
+      <span className={style.titleBottom}>Leader board</span>
+    </h2>
+  </div>
+
+    <ul className={style.list}>{list.map(({ id, name, capital, avatar, isIncrease }) => (<ForbesListItem key={id} name={name} capital={capital} avatar={avatar} isIncrease={isIncrease} />))}</ul>
+</div>
 };
